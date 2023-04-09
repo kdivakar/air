ab2ab81da9a842922361f9278b768943bda15633
sharmila <sharmidiva@gmail.com>
2023-04-09 07:53:57 -0700

    CreateCustomerAPI Updated with all functionalities

--- src/test/java/com/ceta/customer/CreateCustomerAPI.java
+++ src/test/java/com/ceta/customer/CreateCustomerAPI.java
@@ -33,14 +33,14 @@ public class CreateCustomerAPI {
 		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
 				.body(request)
 				.when().post("/cetascyber/customer").then()
-				.assertThat().statusCode(400)
+				.assertThat().statusCode(400).log().all()
 				.extract().response()
 				.as(CreateCustomerResponse.class);
 
 		// Convert POJO to json string, only for debugging
 		Gson gson = new Gson();
 		String jsonString = gson.toJson(response);
-		System.out.println(jsonString);
+		System.out.println("Deserialisation==="+jsonString);
 
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getErrors());
@@ -52,97 +52,190 @@ public class CreateCustomerAPI {
 	}
 
 	@Test
-	public void CreateCustomer_TC002() {
-		//Valid Inputs and name feild with integers
-		RestAssured.baseURI = "http://localhost:8080";
-		String response = given().log().all().header("Content-Type", "application/json")
-				.body("{\r\n" + "  \r\n" + "  \"name\": \"sharmiTC2\",\r\n" + "  \"email\": \""
-						+ UUID.randomUUID().toString() + "@d2.com\",\r\n" + "  \"phoneNumber\": \"121212\"\r\n" + "}")
-				.when().post("/cetascyber/customer").then().extract().response()
-				.asPrettyString();
-		System.out.println("Response of the post method=" + response);
+	public void CreateCustomerWithValidInputIntegers() {
+		//Valid Inputs and name field with integers
 
-		JsonPath js = new JsonPath(response);
-		String customerid = js.getString("com.ceta.customer.customerId");
-		System.out.println("com.ceta.customer id which is extracted==" + customerid);
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setName("sharmila07");
+		request.setEmail(UUID.randomUUID().toString() + "@gmail.com");
+		request.setPhoneNumber("408-222-3222");
+
+		RestAssured.baseURI = URI;
+		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
+				.body(request)
+				.when().post("/cetascyber/customer").then()
+				.assertThat().statusCode(201).log().all()
+				.extract().response()
+				.as(CreateCustomerResponse.class);
+
+		// Convert POJO to json string, only for debugging
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(response);
+		System.out.println("Deserialisation==="+jsonString);
+
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getErrors());
+		Assert.assertNotNull(response.getCustomer());
+		Assert.assertNotNull(response.getCustomer().getId());
+		Assert.assertNotNull(response.getCustomer().getEmail());
+		Assert.assertNotNull(response.getCustomer().getCustomerId());
+		Assert.assertNotNull(response.getCustomer().getName());
+		Assert.assertNotNull(response.getCustomer().getPhoneNumber()); 
 	}
 	@Test
-	public void CreateCustomer_TC003() {
-		//Valid Inputs and name feild with special characters
-		RestAssured.baseURI = "http://localhost:8080";
-		String response = given().log().all().header("Content-Type", "application/json")
-				.body("{\r\n" + "  \r\n" + "  \"name\": \"sharmiTC2@Special\",\r\n" + "  \"email\": \""
-						+ UUID.randomUUID().toString() + "@d2.com\",\r\n" + "  \"phoneNumber\": \"121212\"\r\n" + "}")
-				.when().post("/cetascyber/customer").then().extract().response()
-				.asPrettyString();
-		System.out.println("Response of the post method=" + response);
+	public void CreateCustomerValidInputsSpecialChrs() {
+		//Valid Inputs and name field with special characters
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setName("sharmila07@71");
+		request.setEmail(UUID.randomUUID().toString() + "@gmail.com");
+		request.setPhoneNumber("408-222-3222");
 
-		JsonPath js = new JsonPath(response);
-		String customerid = js.getString("com.ceta.customer.customerId");
-		System.out.println("com.ceta.customer id which is extracted==" + customerid);
+		RestAssured.baseURI = URI;
+		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
+				.body(request)
+				.when().post("/cetascyber/customer").then()
+				.assertThat().statusCode(201).log().all()
+				.extract().response()
+				.as(CreateCustomerResponse.class);
+
+		// Convert POJO to json string, only for debugging
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(response);
+		System.out.println("Deserialisation==="+jsonString);
+
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getErrors());
+		Assert.assertNotNull(response.getCustomer());
+		Assert.assertNotNull(response.getCustomer().getId());
+		Assert.assertNotNull(response.getCustomer().getEmail());
+		Assert.assertNotNull(response.getCustomer().getCustomerId());
+		Assert.assertNotNull(response.getCustomer().getName());
+		Assert.assertNotNull(response.getCustomer().getPhoneNumber()); 
+
 	}
 	@Test
-	public void CreateCustomer_TC004() {
-		//Valid Inputs ,but com.ceta.customer name feild will be repeated
-		//duplicate name feild-same name accepts twice with different email id
-		RestAssured.baseURI = "http://localhost:8080";
-		String response = given().log().all().header("Content-Type", "application/json")
-				.body("{\r\n" + "  \r\n" + "  \"name\": \"sharmiTC2@Special\",\r\n" + "  \"email\": \""
-						+ UUID.randomUUID().toString() + "@d2.com\",\r\n" + "  \"phoneNumber\": \"121212\"\r\n" + "}")
-				.when().post("/cetascyber/customer").then().extract().response()
-				.asPrettyString();
-		System.out.println("Response of the post method=" + response);
+	public void CreateCustomerWithDuplicateName() {
+		//Valid Inputs ,but com.ceta.customer name field will be repeated
+		//duplicate name field-same name accepts twice with different email id
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setName("sharmila07@71");
+		request.setEmail(UUID.randomUUID().toString() + "@gmail.com");
+		request.setPhoneNumber("408-222-3222");
 
-		JsonPath js = new JsonPath(response);
-		String customerid = js.getString("com.ceta.customer.customerId");
-		System.out.println("com.ceta.customer id which is extracted==" + customerid);
+		RestAssured.baseURI = URI;
+		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
+				.body(request)
+				.when().post("/cetascyber/customer").then()
+				.assertThat().statusCode(201).log().all()
+				.extract().response()
+				.as(CreateCustomerResponse.class);
+
+		// Convert POJO to json string, only for debugging
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(response);
+		System.out.println("Deserialisation==="+jsonString);
+
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getErrors());
+		Assert.assertNotNull(response.getCustomer());
+		Assert.assertNotNull(response.getCustomer().getId());
+		Assert.assertNotNull(response.getCustomer().getEmail());
+		Assert.assertNotNull(response.getCustomer().getCustomerId());
+		Assert.assertNotNull(response.getCustomer().getName());
+		Assert.assertNotNull(response.getCustomer().getPhoneNumber()); 
+	
 	}
 	@Test
-	public void CreateCustomer_TC005() {
-		//Valid Inputs ,but com.ceta.customer email feild is missing
-		RestAssured.baseURI = "http://localhost:8080";
-		String response = given().log().all().header("Content-Type", "application/json")
-				.body(" {\r\n"
-						+ "     \"name\": \"sharmilaEmail\",\r\n"
-						+ "    \"email\": \"\",\r\n"
-						+ "    \"phoneNumber\": \"888111\"\r\n"
-						+ "  }")
-				.when().post("/cetascyber/customer").then().extract().response()
-				.asPrettyString();
-		System.out.println("Response of the post method=" + response);
+	public void CreateCustomerWithEmptyEmailId() {
+		//Valid Inputs ,but  email field is missing
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setName("sharmila07@71");
+		request.setEmail("");
+		request.setPhoneNumber("408-222-3222");
 
-		JsonPath js = new JsonPath(response);
-		String customerid = js.getString("com.ceta.customer.customerId");
-		System.out.println("com.ceta.customer id which is extracted==" + customerid);
+		RestAssured.baseURI = URI;
+		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
+				.body(request)
+				.when().post("/cetascyber/customer").then()
+				.assertThat().statusCode(400).log().all()
+				.extract().response()
+				.as(CreateCustomerResponse.class);
+
+		// Convert POJO to json string, only for debugging
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(response);
+		System.out.println("Deserialisation==="+jsonString);
+
+		Assert.assertNotNull(response);
+		Assert.assertNotNull(response.getErrors());
+		Assert.assertNull(response.getCustomer());
+		Assert.assertEquals(response.getErrors().size(), 1);
+		Assert.assertEquals(response.getErrors().get(0).getErrorCode(), "100");
+		Assert.assertEquals(response.getErrors().get(0).getErrorMessage(), "Missing required field");
+		Assert.assertEquals(response.getErrors().get(0).getDetail(), "email");
+	
 	}
 	@Test
-	public void CreateCustomer_TC006() {
+	public void CreateCustomerWithValidPhonenumber() {
 		//Valid Inputs ,but phoneno with strings are passed as inputs
-		RestAssured.baseURI = "http://localhost:8080";
-		String response = given().log().all().header("Content-Type", "application/json")
-				.body("{\r\n" + "  \r\n" + "  \"name\": \"sharmiphone\",\r\n" + "  \"email\": \""
-						+ UUID.randomUUID().toString() + "@d2.com\",\r\n" + "  \"phoneNumber\": \"hhhhaaaa\"\r\n" + "}")
-				.when().post("/cetascyber/customer").then().extract().response()
-				.asPrettyString();
-		System.out.println("Response of the post method=" + response);
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setName("sharmila07");
+		request.setEmail(UUID.randomUUID().toString() + "@gmail.com");
+		request.setPhoneNumber("408-222-rrre");
 
-		JsonPath js = new JsonPath(response);
-		String customerid = js.getString("com.ceta.customer.customerId");
-		System.out.println("com.ceta.customer id which is extracted==" + customerid);
+		RestAssured.baseURI = URI;
+		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
+				.body(request)
+				.when().post("/cetascyber/customer").then()
+				.assertThat().statusCode(201).log().all()
+				.extract().response()
+				.as(CreateCustomerResponse.class);
+
+		// Convert POJO to json string, only for debugging
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(response);
+		System.out.println("Deserialisation==="+jsonString);
+
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getErrors());
+		Assert.assertNotNull(response.getCustomer());
+		Assert.assertNotNull(response.getCustomer().getId());
+		Assert.assertNotNull(response.getCustomer().getEmail());
+		Assert.assertNotNull(response.getCustomer().getCustomerId());
+		Assert.assertNotNull(response.getCustomer().getName());
+		Assert.assertNotNull(response.getCustomer().getPhoneNumber()); 
+	
+
 	}
 	@Test
-	public void CreateCustomer_TC007() {
+	public void CreateCustomerWithEmptyPhoneNumber() {
 		//Valid Inputs ,but phoneno feild is empty
-		RestAssured.baseURI = "http://localhost:8080";
-		String response = given().log().all().header("Content-Type", "application/json")
-				.body("{\r\n" + "  \r\n" + "  \"name\": \"sharmiphonefeildmissing\",\r\n" + "  \"email\": \""
-						+ UUID.randomUUID().toString() + "@d2.com\",\r\n" + "  \"phoneNumber\": \"\"\r\n" + "}")
-				.when().post("/cetascyber/customer").then().extract().response()
-				.asPrettyString();
-		System.out.println("Response of the post method=" + response);
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setName("sharmila07");
+		request.setEmail(UUID.randomUUID().toString() + "@gmail.com");
+		request.setPhoneNumber("");
 
-		JsonPath js = new JsonPath(response);
-		String customerid = js.getString("com.ceta.customer.customerId");
-		System.out.println("com.ceta.customer id which is extracted==" + customerid);
+		RestAssured.baseURI = URI;
+		CreateCustomerResponse response = given().log().all().header("Content-Type", "application/json")
+				.body(request)
+				.when().post("/cetascyber/customer").then()
+				.assertThat().statusCode(201).log().all()
+				.extract().response()
+				.as(CreateCustomerResponse.class);
+
+		// Convert POJO to json string, only for debugging
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(response);
+		System.out.println("Deserialisation==="+jsonString);
+
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getErrors());
+		Assert.assertNotNull(response.getCustomer());
+		Assert.assertNotNull(response.getCustomer().getId());
+		Assert.assertNotNull(response.getCustomer().getEmail());
+		Assert.assertNotNull(response.getCustomer().getCustomerId());
+		Assert.assertNotNull(response.getCustomer().getName());
+		Assert.assertNotNull(response.getCustomer().getPhoneNumber()); 
+
 	}
 }
